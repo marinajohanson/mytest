3f156e05f52815f1958aa28a9a2ee518e6e16826
marinajohanson <150165314+marinajohanson@users.noreply.github.com>
2023-11-07 21:05:50 +0300

    卐
    did something

--- cockk.cpp
+++ cockk.cpp
@@ -1,31 +1,89 @@
 #include <iostream>
+#include <random>
+#include<Windows.h> // for clear()
 using namespace std;
+
+
+
+void clear() {
+    COORD topLeft  = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(
+        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    FillConsoleOutputAttribute(
+        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+        screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+    );
+    SetConsoleCursorPosition(console, topLeft);
+}
+
+//clears console
+
+
+
+
+
 
 int main()
 {
-    int nigger[] = {};        // there's supposed to be some numbers
 
-    for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)        // How does it works!?卐卐卐卐
-    {                                                // How does it works!?卐卐卐卐
-        for (int j = 0; j < sizeof(nigger)/sizeof(nigger[0]) - 1; j++)        // How does it works!?卐卐卐卐
-        {                                        // How does it works!?卐卐卐卐
-            if (nigger[j] < nigger[j+1])            // How does it works!?卐卐卐卐
-            {                                    // How does it works!?卐卐卐卐
-                int temp = nigger[j];            // How does it works!?卐卐卐卐   
-                nigger[j] = nigger[j+1];        // How does it works!?卐卐卐卐
-                nigger[j+1] = temp;             // How does it works!?卐卐卐卐
-            }                                            // How does it works!?卐卐卐卐
-            
-        }                                                // How does it works!?卐卐卐卐
-        
-    }                                                // How does it works!?卐卐卐卐
+//  RANDOM
+    random_device rd;
+    mt19937 rand(rd());           
+    uniform_int_distribution<int> dist(0, MAXINT);
+// RANDOM
 
+    clear();        // clears console
+
+    const int arraynum = 100;               // ciferki
+
+    long nigger[arraynum];
+
+    for (int i = 0; i < arraynum; i++)
+    {
+        nigger[i] = dist(rand);             // generates random numbers to array
+    }
 
     
 
     for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)
     {
-        cout << nigger[i] << ' ';            // prints this array
+        cout << nigger[i] << ' ';                                                       // current array
+    }
+    
+    cout << "\n\n*********************************************************************************************************\n\n";
+
+
+
+
+
+
+
+    for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)                                          // BUBBLE SORT
+    {
+        for (int j = 0; j < sizeof(nigger)/sizeof(nigger[0]) - 1; j++)
+        {
+            if (nigger[j] < nigger[j+1])
+            {
+                int temp = nigger[j];               
+                nigger[j] = nigger[j+1];        // Replaces the bigger number to nigger[j], but anyway idk how it's working
+                nigger[j+1] = temp;             
+            }
+        }
+    }
+
+
+
+
+
+    for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)
+    {
+        cout << nigger[i] << ' ';                                                   // after sorting array
     }
     
     
