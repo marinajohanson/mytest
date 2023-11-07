045ca5c48605934bc2eda1f0c568239891c51fd0
marinajohanson <150165314+marinajohanson@users.noreply.github.com>
2023-11-07 20:18:14 +0300

    Update cockk.cpp
    卐

--- cockk.cpp
+++ cockk.cpp
@@ -3,26 +3,29 @@ using namespace std;
 
 int main()
 {
-    int nigger[] = {};
+    int nigger[] = {};        // there's supposed to be some numbers
 
-    for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)
-    {
-        for (int j = 0; j < sizeof(nigger)/sizeof(nigger[0]) - 1; j++)
-        {
-            if (nigger[j] < nigger[j+1])
-            {
-                int temp = nigger[j];               
-                nigger[j] = nigger[j+1];        // Replaces the bigger number to nigger[j], but anyway idk how it's working
-                nigger[j+1] = temp;             
-            }
+    for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)        // How does it works!?卐卐卐卐
+    {                                                // How does it works!?卐卐卐卐
+        for (int j = 0; j < sizeof(nigger)/sizeof(nigger[0]) - 1; j++)        // How does it works!?卐卐卐卐
+        {                                        // How does it works!?卐卐卐卐
+            if (nigger[j] < nigger[j+1])            // How does it works!?卐卐卐卐
+            {                                    // How does it works!?卐卐卐卐
+                int temp = nigger[j];            // How does it works!?卐卐卐卐   
+                nigger[j] = nigger[j+1];        // How does it works!?卐卐卐卐
+                nigger[j+1] = temp;             // How does it works!?卐卐卐卐
+            }                                            // How does it works!?卐卐卐卐
             
-        }
+        }                                                // How does it works!?卐卐卐卐
         
-    }
+    }                                                // How does it works!?卐卐卐卐
+
+
+    
 
     for (int i = 0; i < sizeof(nigger)/sizeof(nigger[0]); i++)
     {
-        cout << nigger[i] << ' ';
+        cout << nigger[i] << ' ';            // prints this array
     }
     
     
